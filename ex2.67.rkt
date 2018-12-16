4143852b0c2ebe6e1ae65bd11095d64426cead55
YangLiu <liuy7937@gmail.com>
2018-12-16 19:05:48 +0800

    commit some solutions.

--- ex2.67.rkt
+++ ex2.67.rkt
@@ -1,5 +1,20 @@
-#lang racket
+#lang SICP
+#| tree相关函数 |#
+(define (left-branch tree) (car tree))
 
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+#| leaf相关函数 |#
 (define (make-leaf symbol weight)
   (list 'leaf symbol weight))
 
@@ -10,48 +25,56 @@
 
 (define (weight-leaf x) (caddr x))
 
+#| append函数 |#
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+#| 生成霍夫曼编码树 |#
 (define (make-code-tree left right)
   (list left
         right
         (append (symbols left) (symbols right))
         (+ (weight left) (weight right))))
 
-(define (left-branch tree)
-  (car tree))
+#| 样例树 |#
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+#| 样例消息 |#
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-(define (right-branch tree)
-  (cadr tree))
-
-(define (symbols tree)
-  (if (leaf? tree)
-      (list (symbol-leaf tree))
-      (caddr tree)))
-
-(define (weight tree)
-  (if (leaf? tree)
-      (list (weight-leaf tree))
-      (cadddr tree)))
-
+#| 解码相关函数 |#
 (define (decode bits tree)
   (define (decode-1 bits current-branch)
     (if (null? bits)
         '()
-        (let ((next-branch (choose-branch (car bits) current-branch)))
-           (if (leaf? next-branch)
-               (cons (symbol-leaf next-branch)
-                     (decode-1 (cdr bits) tree))
-               (decode-1 (cdr bits) next-branch)))))
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
-        
 
 (define (choose-branch bit branch)
   (cond ((= bit 0) (left-branch branch))
         ((= bit 1) (right-branch branch))
         (else (error "bad bit -- CHOOSE-BRANCH" bit))))
 
-(define (adjoin-set x set)
-  (cond ((null? set) (list x))
-        ((< (weight x) (weight (car set))) (cons x set))
-        (else (cons (car set)
-                    (adjoin-set x (cdr set))))))
+#| 解码操作 |#
+(decode sample-message sample-tree)
+
+
+
+
+
+
+
+
+
 
